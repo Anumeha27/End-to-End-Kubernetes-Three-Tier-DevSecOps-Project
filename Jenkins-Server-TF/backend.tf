7cb757101a32a48104dfd98115461e96494ea038
Anumeha27 <74310949+Anumeha27@users.noreply.github.com>
2026-05-11 19:14:17 +0530

    Updated locking mechanism to bucket
    dynamodb is not required anymore

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "mern-tfstate-bucket"
+    region         = "eu-north-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    use_lockfile = true
   }
   required_version = ">=0.13.0"
   required_providers {
